cba9b714082f0a05ec52434314f2f761efb9bc41
Arun Sood <arun.sood@coursehero.com>
2018-10-09 00:23:51 -0700

    Added documentation

--- index.js
+++ index.js
@@ -54,4 +54,8 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
-app.listen(3000);
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}...`);
+});
